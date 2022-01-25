5c77b3bce91d6954c724aee5bd46d9130422afa3
jmurphy5613 <jmurphy5613@gmail.com>
2022-01-25 08:59:15 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
